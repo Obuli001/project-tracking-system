4b6118e51527f6d58ca8e41c026eb27384d264ea
obuli karthikeyan <56989773+Obuli001@users.noreply.github.com>
2020-07-19 02:02:56 -0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjectTrackingSystem
+{
+    public partial class Login : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-V5HGEU3\\SQLEXPRESS;Initial Catalog=ProjectTracking;Persist Security Info=True;User ID=sa;Password=123");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dr;
+
+        string name, fid;
+        public static String id;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Registration r1 = new Registration();
+            r1.ShowDialog();
+           
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            id = txtId.Text;
+            string pwd = txtPwd.Text;
+
+            if (id.Equals("admin"))
+            {
+                AddProjects objUI = new AddProjects();
+                objUI.Show();
+            }
+            else
+            {
+
+
+                cmd = new SqlCommand("select * from register where emp_Id='" + id + "' and Password='" + pwd + "'", con);
+                con.Open();
+                dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    MessageBox.Show("Login success!");
+
+                    MyProject objUI = new MyProject();
+                    objUI.Show();
+
+                }
+                else
+                {
+                    MessageBox.Show("Invalid details!");
+                }
+                con.Close();
+            }
+        }
+    }
+}
---
+++ MyProject.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjectTrackingSystem
+{
+
+    public partial class MyProject : Form
+    {
+
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-V5HGEU3\\SQLEXPRESS;Initial Catalog=ProjectTracking;Persist Security Info=True;User ID=sa;Password=123");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+
+        public MyProject()
+        {
+            InitializeComponent();
+        }
+
+        private void MyProject_Load(object sender, EventArgs e)
+        {
+            String eid = "c2";
+            //cmd = new SqlCommand("Select EnquiryNo,EnqDate,StudentName,College,Degree,Dept,Year,MobileNo,MobileNo1,MailId,MailId1 from Enquiry where EnqDate='" + lblDate.Text + "' and status=0", con);
+            cmd = new SqlCommand("Select pid,name,DOS,language,periority from projects where assign='" + eid + "'", con);
+
+            da = new SqlDataAdapter(cmd);
+            ds = new DataSet();
+            da.Fill(ds, "projects");
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "projects";
+
+            //txtStat.Text=
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String eid = "c2";
+            String pid=lblPid.Text;
+            String stat = edtStat.Text;
+            cmd = new SqlCommand("insert into assignment values('" + eid + "','" + pid + "','" + stat + "')", con);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+
+        //private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        //{
+        //    int i;
+        //    i = e.RowIndex;
+        //    lblPid.Text = dataGridView1.Rows[i].Cells[0].Value.ToString();
+        //    lblDos.Text = dataGridView1.Rows[i].Cells[2].Value.ToString();
+        //}
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            i = e.RowIndex;
+            lblPid.Text = dataGridView1.Rows[i].Cells[0].Value.ToString();
+            lblDos.Text = dataGridView1.Rows[i].Cells[2].Value.ToString();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            i = e.RowIndex;
+            lblPid.Text = dataGridView1.Rows[i].Cells[0].Value.ToString();
+            lblDos.Text = dataGridView1.Rows[i].Cells[2].Value.ToString();
+        }
+
+    }
+}
